ad72c1a9124beb676926b17e7cecdeb0fcd4b188
GULSHAN Kumar <67850876+gulshan8383@users.noreply.github.com>
2024-07-10 14:32:15 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://todo-list-with-dark-butoon.onrender.com',
 });
 
 export default api;
